1f0187a5dd8b3d93eaae0ba75970c0661f10449d
ojbgrsx <bolsunbekov.akylbek@gmail.com>
2021-12-01 01:44:54 +0600

    Task and completed files

--- testing.py
+++ testing.py
@@ -1,25 +1,52 @@
+from datetime import date
 from google_api import *
 from menus import *
 
-name = 'akylbek'
-splitted = []
-unchanged = []
+name = 'akimaaly'
+len_name = len(name)
+completed = []
+uncompleted = []
 task = open('tasks.txt', 'r')
-for k in task:
-    if 'AKYLBEK' in k:
-        s = ''
-        s += k
-        print(s, end='')
-        unchanged.append(s)
-        splitted.append(k.split())
 print()
-print(unchanged)
-for i in splitted:
-    del i[0:4]
-    del i[-3::]
+for k in task:
+    s = ''
+    s += k
+    if name.upper() in k:
+        completed.append(s)
+    else:
+        uncompleted.append(s)
+pprint(completed)
+pprint(uncompleted)
+print()
+for qwerty in range(len(completed)):
+    print(qwerty+1, completed[qwerty], end='')
+print()
+task.close()
+task = open('tasks.txt', 'r', encoding='utf-8')
+choosing = input('Please choose the work that you want to finish: ')
+print()
+perdun = completed[int(choosing)-1]
 
-print(splitted)
-# print('Please type the FIRST WORD of the work to COMPLETE\n')
+s = 'Done by {} : '.format(
+    name.upper()) + perdun[12+len_name:-32] + ' >>> at {}\n'.format(datetime.datetime.now())
+completed.append(s)
+uncompleted.clear()
+for k in task:
+    if completed[int(choosing)-1] != k:
+        uncompleted.append(k)
+del completed[0:-1]
+pprint(completed)
+pprint(uncompleted)
+task.close()
+
+task = open('tasks.txt', 'w', encoding='utf-8')
+task.writelines(uncompleted)
+task.close()
+
+complete = open('completed.txt', 'a', encoding='utf-8')
+complete.writelines(completed)
+complete.close()
+
 # q = input()
 # uncompleted = []
 # completed = ['Done by {}: '.format(name.upper())]
